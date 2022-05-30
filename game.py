98052fccbfecda271842d8a5deaad22a3d373117
Jack Chen <jackczy1209@gmail.com>
2022-05-30 15:42:35 -0700

    Initial Commit

---
+++ game.py
@@ -0,0 +1,35 @@
+
+
+
+def game():
+
+    # initialize game and players
+
+    # while game
+        # play turn
+        # switch player
+
+    pass
+
+
+def turn(player):
+    # handle jail
+
+    # handle housing
+
+    # roll dice
+
+    # move
+
+    # action
+        # if housing
+            # check if bought: what are the rents
+            # else ask if want to buy
+        # if special card:
+            # railroad
+            # utility
+        # if land in jail
+        # if
+
+
+    pass
