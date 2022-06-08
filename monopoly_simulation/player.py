b26556893559bb912141499e275c8104500fb946
Jack Chen <jackczy1209@gmail.com>
2022-06-07 19:49:15 -0700

    more handles for round_action (jail, properties)

--- monopoly_simulation/player.py
+++ monopoly_simulation/player.py
@@ -1,23 +1,38 @@
 
 
-class PlayerActionsHandler:
+class PlayerActionsHandler():
     def jail_card_handler(self):
+        return True
+
+    def jail_actions_handler(self):
+        return True
+
+    def buy_property_handler(self):
+        return True
+
+    def debt_handler(self,properties):
         return True
 
 class Player(PlayerActionsHandler):
     id = 0
     properties_owned = []
     money = 0
-    in_jail = False
     rounds_in_jail = 0
     mortgages = []
     location = ""
     jail_card = False
 
-    def use_jail_card(self):
-        if self.jail_card:
-            return self.jail_card_handler()
-            # prompt user response to use or not
+    def jail_round(self):
+        if self.jail_card_handler():
+            self.jail_card = False
+            return True
         else:
-            return False
+            if self.jail_actions_handler():
+                self.money -= 50
+                if self.money < 0:
+                    self.on_debt(0-self.money)
+                return True
 
+
+    def on_debt(self, amount):
+        pass
--- monopoly_simulation/tile.py
+++ monopoly_simulation/tile.py
@@ -1,32 +1,71 @@
 import abc
+from . import utility
+from . import game
 
 
 class Tile:
     owner = ""
+    is_owned = False
 
-    def round_action(self, **kwarg):
+
+    def round_action(self, player, **kwarg):
         pass
 
 
 
 class Property(Tile):
-    def round_action(self, **kwarg):
-        pass # give option to buy or pay
+
+    price = 0
+    base_rent = 0
+    premium_rents = [] # 0 being set owned, 5 being hotel, rest being their respective number of houses
+    set = ""
+    # note: railroads/utility work differently
+
+    def calculate_rent(self):
+        pass
+
+    def round_action(self, player, **kwarg):
+        if self.is_owned:
+            rent = self.calculate_rent() # calculate rents
+            # apply rent to player
+        else:
+            if player.buy_property_handler():
+                # buy prpoerty
+                player.money -= self.price
+                self.owner = player
+            else:
+                auction_players = []
+                for p in kwarg.get('game_state').players:
+                    if not player.__eq__(p):
+                        auction_players.append(p)
+                self.owner = utility.auction(participants=auction_players,minimum_bid=self.price)
+
+
 
 
 class Jail(Tile):
-    def round_action(self, **kwarg):
-        player = kwarg.get('player')
-        if not player.use_jail_card():
-            # land player
-            player.in_jail = True
-            player.rounds_in_jail = 3
+    def round_action(self, player, **kwarg):
+        player.rounds_in_jail = 3
+        player.position = 10
 
 
 
 
 class Street(Property):
-    pass
+    houses = 0
+    housing_cost = 0
+    hotel_cost = 0
+    hotel = 0
+
+    def calculate_rent(self):
+        # check if set is owned
+        set_owned = False
+
+        if set_owned:
+            if self.hotel>0: return self.premium_rents[5]
+            else: return self.premium_rents[0]
+
+
 
 
 class Public_property(Property):
